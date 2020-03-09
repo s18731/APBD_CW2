ffce2aa7609d32b4951a32012c6ebbd228e10127
s18731 <s18731@LAB003-07>
2020-03-09 11:29:52 +0100

    Hello

---
+++ ConsoleApp1/ConsoleApp1/Modele/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1.Modele
+{
+    public class Student
+    {
+        //prop + tabx2
+        [XmlElement(elementName:"fname")]
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+        [XmlAttribute(attributeName:"email")]
+        public string Email { get; set; }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Wczytywanie pliku
+            string FilePath = @"C:\Users\s18731\Desktop\APBD_CW2-master\ConsoleApp1\ConsoleApp1\Data\dane.csv";
+
+            FileInfo f = new FileInfo(FilePath);
+            using (StreamReader stream = new StreamReader(f.OpenRead()))
+            {
+                string line = "";
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] studentWiersz = line.Split(',');
+                    Console.WriteLine(line);
+                }
+            }
+            FileStream witer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Modele.Student>), 
+                                        new XmlRootAttribute("uczelnia"));
+            var list = new List<Modele.Student>();
+            var st = new Modele.Student
+            {
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "kowalski@email.pl"
+            };
+            list.Add(st);
+            serializer.Serialize(witer, list);
+        }
+    }
+}
